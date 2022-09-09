3cbdb63076018c06f4abc52058f397937241381e
Your Name <you@example.com>
2022-09-08 23:49:39 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  * main - Print exactly
@@ -7,7 +6,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
