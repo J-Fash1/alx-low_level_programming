2f8596c0c72ccf86a075f74c304d39882cf1c425
Your Name <you@example.com>
2022-09-09 00:46:12 -0700

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the size of various types on the computer it is compiled and run on
+ * main - Prints the size of various types on the computer
  *
  * Return: Always 0 (Success)
  */
@@ -11,6 +11,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
 	printf("Size of an int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
